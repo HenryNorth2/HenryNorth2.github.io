af883151a49ed4e0c5ec00829edd333beb5e765e
Henry North <henrynorth@outlook.com>
2020-04-06 16:53:38 +0100

    add routes and base pages for estate agency website

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
-import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { PropertiesComponent } from './components/properties/properties.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'properties', component: PropertiesComponent },
+  { path: 'about-us', component: AboutComponent },
+  { path: 'contact-us', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +27,18 @@ import { AddTodoComponent } from './components/add-todo/add-todo.component';
     TodosComponent,
     TodoItemComponent,
     HeaderComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent,
+    FooterComponent,
+    ContactComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
