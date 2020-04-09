fc181cdf34da1fd9a3f286ef672e2fcf4cb44fdc
Henry North <henrynorth@outlook.com>
2020-04-09 19:17:35 +0100

    added base for properties page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
@@ -15,6 +23,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarouselItemComponent } from './components/home/carousel-item/carousel-item.component';
+import { PropertyItemComponent } from './components/properties/property-item/property-item.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -35,14 +44,22 @@ const routes: Routes = [
     ContactComponent,
     PropertiesComponent,
     HomeComponent,
-    CarouselItemComponent
+    CarouselItemComponent,
+    PropertyItemComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
