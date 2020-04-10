f3621453706c576d9fc3900dca237552e52e754c
Henry North <henrynorth@outlook.com>
2020-04-10 02:39:54 +0100

    page not found component and tests for property details page

--- app/src/app/models/Property.ts
+++ app/src/app/models/Property.ts
@@ -1,52 +1,52 @@
 export class Property {
   propertyId: number;
   listingId: string;
-  details: {
-    propertyType: string;
-    status: string;
-    dateUpdated: string;
-    shortDescription: string;
-    fullDescription: string;
-    bathrooms: number;
-    bedrooms: number;
-    receptionRooms: number;
-    price: number;
-    priceQualifier: string;
-    latitude: number;
-    longitude: number;
-    displayAddress: string;
-    creatingAgentId: string;
-    address: {
-      udprn: number;
-      paon: string;
-      saon: string;
-      street: string;
-      locality: string;
-      town: string;
-      district: string;
-      county: string;
-      postcode: string
-    };
-    propertyAttributes: [
-      {
-        id: number;
-        type: number;
-        valueText: string;
-        valueBoolean: boolean;
-        valueInt: number;
-        valueDecimal: number
-      }
-    ];
-    features: [
-      string
-    ];
-    media: [
-      {
-        dateUpdated: string;
-        url: string;
-        mediaType: string;
-        mimeType: string
-      }
-    ]
-  }
+  // details: {
+  //   propertyType: string;
+  //   status: string;
+  //   dateUpdated: string;
+  //   shortDescription: string;
+  //   fullDescription: string;
+  //   bathrooms: number;
+  //   bedrooms: number;
+  //   receptionRooms: number;
+  //   price: number;
+  //   priceQualifier: string;
+  //   latitude: number;
+  //   longitude: number;
+  //   displayAddress: string;
+  //   creatingAgentId: string;
+  //   address: {
+  //     udprn: number;
+  //     paon: string;
+  //     saon: string;
+  //     street: string;
+  //     locality: string;
+  //     town: string;
+  //     district: string;
+  //     county: string;
+  //     postcode: string
+  //   };
+  //   propertyAttributes: [
+  //     {
+  //       id: number;
+  //       type: number;
+  //       valueText: string;
+  //       valueBoolean: boolean;
+  //       valueInt: number;
+  //       valueDecimal: number
+  //     }
+  //   ];
+  //   features: [
+  //     string
+  //   ];
+  //   media: [
+  //     {
+  //       dateUpdated: string;
+  //       url: string;
+  //       mediaType: string;
+  //       mimeType: string
+  //     }
+  //   ]
+  // }
 }
